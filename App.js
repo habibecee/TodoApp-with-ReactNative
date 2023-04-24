887fe467a83519ec6bed06651d972c82251adfe8
habibecee <habibe.ce1996@gmail.com>
2023-04-24 16:03:06 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import Header from './src/components/header';
 import GeneralStyles from './src/utils/generalStyles';
+import Icon from 'react-native-vector-icons/AntDesign';
 function App() {
   return (
     <SafeAreaView style={GeneralStyles.flex1}>
       <Header title="My Todo App" />
+      <View>
+        <Text> TODO APP WEEK 1</Text>
+        <Icon name="pluscircle" size={25} color="red" />
+      </View>
     </SafeAreaView>
   );
 }
